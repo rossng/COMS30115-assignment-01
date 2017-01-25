7213a4db3891549e81069fb68842171ff9c8d00e
Ross Gardiner <ross-public@gmx.com>
2017-01-25 21:21:31 +0000

    Reformat code

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -13,55 +13,50 @@ using glm::mat3;
 
 const int SCREEN_WIDTH = 500;
 const int SCREEN_HEIGHT = 500;
-SDL_Surface* screen;
+SDL_Surface *screen;
 int t;
 
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
 
 void Update();
+
 void Draw();
 
-int main( int argc, char* argv[] )
-{
-	screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT );
-	t = SDL_GetTicks();	// Set start value for timer.
+int main(int argc, char *argv[]) {
+    screen = InitializeSDL(SCREEN_WIDTH, SCREEN_HEIGHT);
+    t = SDL_GetTicks();    // Set start value for timer.
 
-	while( NoQuitMessageSDL() )
-	{
-		Update();
-		Draw();
-	}
+    while (NoQuitMessageSDL()) {
+        Update();
+        Draw();
+    }
 
-	SDL_SaveBMP( screen, "screenshot.bmp" );
-	return 0;
+    SDL_SaveBMP(screen, "screenshot.bmp");
+    return 0;
 }
 
-void Update()
-{
-	// Compute frame time:
-	int t2 = SDL_GetTicks();
-	float dt = float(t2-t);
-	t = t2;
-	cout << "Render time: " << dt << " ms." << endl;
+void Update() {
+    // Compute frame time:
+    int t2 = SDL_GetTicks();
+    float dt = float(t2 - t);
+    t = t2;
+    cout << "Render time: " << dt << " ms." << endl;
 }
 
-void Draw()
-{
-	if( SDL_MUSTLOCK(screen) )
-		SDL_LockSurface(screen);
+void Draw() {
+    if (SDL_MUSTLOCK(screen))
+        SDL_LockSurface(screen);
 
-	for( int y=0; y<SCREEN_HEIGHT; ++y )
-	{
-		for( int x=0; x<SCREEN_WIDTH; ++x )
-		{
-			vec3 color( 1.0, 0.0, 0.0 );
-			PutPixelSDL( screen, x, y, color );
-		}
-	}
+    for (int y = 0; y < SCREEN_HEIGHT; ++y) {
+        for (int x = 0; x < SCREEN_WIDTH; ++x) {
+            vec3 color(1.0, 0.0, 0.0);
+            PutPixelSDL(screen, x, y, color);
+        }
+    }
 
-	if( SDL_MUSTLOCK(screen) )
-		SDL_UnlockSurface(screen);
+    if (SDL_MUSTLOCK(screen))
+        SDL_UnlockSurface(screen);
 
-	SDL_UpdateRect( screen, 0, 0, 0, 0 );
+    SDL_UpdateRect(screen, 0, 0, 0, 0);
 }
